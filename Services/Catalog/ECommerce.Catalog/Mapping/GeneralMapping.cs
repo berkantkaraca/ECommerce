8cbe44272039db2d9dc9268aedb85a288f32cd66
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-10 23:00:03 +0300

    add catalog api

---
+++ Services/Catalog/ECommerce.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ECommerce.Catalog.Dtos.CategoryDtos;
+using ECommerce.Catalog.Dtos.ProductDetailDtos;
+using ECommerce.Catalog.Dtos.ProductDtos;
+using ECommerce.Catalog.Dtos.ProductImageDtos;
+using ECommerce.Catalog.Entities;
+
+namespace ECommerce.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIdProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductsWithCategoryDto>().ReverseMap();
+
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIdProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+
+            CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, GetByIdProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Catalog/ECommerce.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string FeatureSliderCollectionName { get; set; }
+        public string ProductDetailsCollectionName { get; set; }
+        public string ProductImagesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
