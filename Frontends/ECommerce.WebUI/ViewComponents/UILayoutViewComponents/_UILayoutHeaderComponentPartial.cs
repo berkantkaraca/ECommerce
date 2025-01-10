731b7c3d7be9ee178b85b78d65911e7cf896bf8a
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-10 18:17:22 +0300

    add uı layout

---
+++ Frontends/ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutHeaderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
