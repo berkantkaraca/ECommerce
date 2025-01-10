7908c72c41f4a7b88c698a78b3d02b5436c39325
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-10 19:20:00 +0300

    add product detail page

---
+++ Frontends/ECommerce.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
