1090e44b2dc73f64de4f19494a161135c1cfdc1c
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-11 10:21:31 +0300

    add discount api

---
+++ Services/Discount/ECommerce.Discount/Context/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ECommerce.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1444;initial catalog=DiscountDb;User=sa;Password=123456aA*");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Services/Discount/ECommerce.Discount/Program.cs
@@ -0,0 +1,38 @@
+using ECommerce.Discount.Context;
+using ECommerce.Discount.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<DapperContext>();
+builder.Services.AddTransient<IDiscountService, DiscountService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.RequireHttpsMetadata = false;
+    opt.Audience = "ResourceDiscount";
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
