95bd7b896d04c9fe42f3903b9128155db5acab52
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-13 14:39:06 +0300

    add category crud implemention on admin panel

---
+++ Frontends/ECommerce.DtoLayer/CatalogDtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Frontends/ECommerce.DtoLayer/CatalogDtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class UpdateCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
