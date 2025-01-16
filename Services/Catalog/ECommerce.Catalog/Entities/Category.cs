f82410a660326dee5a3707b9f43af31f72b6aeda
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-16 11:45:20 +0300

    add ImageUrl to category entities and list categories on uı

--- Services/Catalog/ECommerce.Catalog/Entities/Category.cs
+++ Services/Catalog/ECommerce.Catalog/Entities/Category.cs
@@ -9,5 +9,6 @@ namespace ECommerce.Catalog.Entities
         [BsonRepresentation(BsonType.ObjectId)]
         public string CategoryId { get; set; }
         public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
