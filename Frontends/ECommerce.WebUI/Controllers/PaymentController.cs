b659836ff9c1f7509a7d508917614756ec0447d9
Berkant Karaca <berkantkaracatr@gmail.com>
2025-03-06 18:47:10 +0300

    add payment page

---
+++ Frontends/ECommerce.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
